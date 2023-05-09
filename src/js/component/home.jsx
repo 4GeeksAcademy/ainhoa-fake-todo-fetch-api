785f26bf72be2942dccd3a0f884d6da4957b57a3
Ainhoa <113107823+AinhoaQM@users.noreply.github.com>
2023-05-09 15:50:00 +0000

    funcionalidad hecha, falta maquetar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [todos, setTodos] = useState([])
+	const [input, setInput] = useState("")
+	useEffect(()=>{
+		getAllTodos()
+	},[])
+
+	const getAllTodos = () => {
+		let requestOptions = {
+			method: 'GET',
+			redirect: 'follow'
+		  };
+		  
+		  fetch("https://assets.breatheco.de/apis/fake/todos/user/ainhoaQM", requestOptions)
+			.then(response => response.json())
+			.then(result => setTodos(result))
+			.catch(error => console.log('error', error));
+	}
+
+	const addTodo = () => {
+		let myHeaders = new Headers();
+         myHeaders.append("Content-Type", "application/json");
+
+
+		 let agregar = todos.concat( {
+			"label": input,
+			"done": false
+		  })
+
+         let requestOptions = {
+           method: 'PUT',
+           headers: myHeaders,
+           body: JSON.stringify(agregar),
+           redirect: 'follow'
+         };
+
+        fetch("https://assets.breatheco.de/apis/fake/todos/user/ainhoaQM", requestOptions)
+          .then(response => response.json())
+          .then(result => getAllTodos())
+          .catch(error => console.log('error', error));
+	}
+
+	const deleteTodo = (todo) => {
+		let myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
+
+
+		let eliminar = todos.filter(task => task != todo)
+		console.log(eliminar)
+
+		let requestOptions = {
+		  method: 'PUT',
+		  headers: myHeaders,
+		  body: JSON.stringify(eliminar),
+		  redirect: 'follow'
+		};
+
+	   fetch("https://assets.breatheco.de/apis/fake/todos/user/ainhoaQM", requestOptions)
+		 .then(response => response.json())
+		 .then(result => getAllTodos())
+		 .catch(error => console.log('error', error));
+		   }
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<input type="text" value={input} onChange={(e)=>setInput(e.target.value)} placeholder="What do yo do?"></input>
+			<button onClick={addTodo}>Send</button>
+			
+			<div>
+			{todos.map((todo, index)=>(
+				<>
+					<p key={index}>
+						{todo.label}
+					</p>
+					<button onClick={()=>deleteTodo(todo)} className="btn btn-danger">
+						Eliminar
+					</button>
+				</>
+			))}
+			</div>
 		</div>
 	);
 };
